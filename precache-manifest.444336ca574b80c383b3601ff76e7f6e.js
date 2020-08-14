72457d71a5b43c95bcdc83fb0a454ecfef0af969
Neil <huynhnguyenv2@gmail.com>
2020-08-14 14:43:59 +0700

    Updates

---
+++ precache-manifest.444336ca574b80c383b3601ff76e7f6e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b4a668a6d5ff9e77dc0c9b76cb88efd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0dea1d539ed336f4ed1d",
+    "url": "/static/css/2.a63cf73e.chunk.css"
+  },
+  {
+    "revision": "055d4ced025ef4f2ead1",
+    "url": "/static/css/main.cdb38684.chunk.css"
+  },
+  {
+    "revision": "0dea1d539ed336f4ed1d",
+    "url": "/static/js/2.1d4c032f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1d4c032f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "055d4ced025ef4f2ead1",
+    "url": "/static/js/main.b6244999.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
